44d2f258dcb0514275b8b14838cf77683a0eaaa7
Gouri Shankar Kumawat <gskumawat555@gmail.com>
2019-06-06 15:56:38 +0530

    add markdown previewer functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import marked from 'marked';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+class App extends Component {  
+  state={
+    contents: [],
+    text: '',
+    content: ''
+  }
+
+  handleChange = e =>{
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+
+  handlePublish = e =>{
+    // debugger
+    // let content =  e.target.value.split('.');
+    this.setState({
+      text: '',
+      content: this.state.content.concat('\n', marked(this.state.text))
+    })
+  }
+  
+  render(){
+    let {text, content} = this.state;
+    // console.log(marked)
+    // debugger
+    return (
+      <div className="App">
+      <header className='header'>
+        <h1>Responsive Markdown</h1>
       </header>
+        <textarea className='textarea' onChange={this.handleChange} value={text} rows={15} name='text'>
+          
+        </textarea>
+        <button onClick={this.handlePublish}>Publish</button>
+        <div className='content' dangerouslySetInnerHTML={{__html: content}}>
+          
+        </div>
     </div>
   );
+  }
 }
 
 export default App;
